f5539b96d1cf6ecf2b1e4ee6fc291b1e65bc4b2c
valeamoris <ctt@zjmax.com>
2025-09-08 10:25:23 +0800

    refactor(BSCValidatorSet): make BLOCK_AWARD constant
    - Change BLOCK_AWARD from a public variable to a public constant
    - This ensures that the block award value cannot be modified after deployment

--- contracts/BSCValidatorSet.sol
+++ contracts/BSCValidatorSet.sol
@@ -60,7 +60,7 @@ contract BSCValidatorSet is IBSCValidatorSet, System, IParamSubscriber, IApplica
     uint256 public constant MAX_SYSTEM_REWARD_BALANCE = 100 ether;
 
     // Mercury upgrade
-    uint256 public const BLOCK_AWARD = 1500000000000000000; // 1.5 ether in wei
+    uint256 public constant BLOCK_AWARD = 1500000000000000000; // 1.5 ether in wei
 
     uint256 public systemRewardBaseRatio;
     uint256 public previousHeight;
